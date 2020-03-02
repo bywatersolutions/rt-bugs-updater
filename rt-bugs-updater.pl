9c80a37470bc5de36ffc8a83761d72f775763eb5
Kyle M Hall <kyle@bywatersolutions.com>
2020-03-02 11:29:00 -0500

    Loop over each Queue, RT doesn't like OR searches using Queues

--- rt-bugs-updater.pl
+++ rt-bugs-updater.pl
@@ -55,61 +55,67 @@ catch {
     die "Problem logging in: ", shift->message;
 };
 
+my @queues = ( 'Bugs', 'Support' );
+foreach my $q ( @queues ) {
 
-# Create tracks
-say colored( 'Finding bug tickets', 'green' ) if $verbose;
-my $rt_query = q{ Status = '__Active__' AND ( Queue = 'Bugs' OR Queue = 'Support' ) };
-my @ids = $rt->search(
-    type    => 'ticket',
-    query   => $rt_query,
-    orderby => '-id',
-);
+    # Create tracks
+    say colored( "Finding '$q' tickets", 'green' ) if $verbose;
 
-my @tickets;
+    my $rt_query = qq{ Status = '__Active__' AND Queue = '$q' };
+    my @ids = $rt->search(
+        type    => 'ticket',
+        query   => $rt_query,
+        orderby => '-id',
+    );
 
-foreach my $ticket_id (@ids) {
-    sleep(1);    # pause for 1 second between requests so we don't kill RT
-    my $ticket = $rt->show( type => 'ticket', id => $ticket_id );
+    my @tickets;
 
-    say "Working on ticket " . colored( $ticket_id, 'cyan' ) if $verbose > 1;
-    my ($bug_id, $others) = split(',', $ticket->{'CF.{Community Bug}'} );
+    foreach my $ticket_id (@ids) {
+        sleep(2);    # pause between requests so we don't kill RT
+        my $ticket = $rt->show( type => 'ticket', id => $ticket_id );
 
-    $bug_id ||= q{};
-    $others ||= q{};
+        say "Working on ticket " . colored( $ticket_id, 'cyan' ) if $verbose > 1;
+        my ($bug_id, $others) = split(',', $ticket->{'CF.{Community Bug}'} );
 
-    $bug_id =~ s/^\s+|\s+$//g;
-    $others =~ s/^\s+|\s+$//g;
+        $bug_id ||= q{};
+        $others ||= q{};
 
-    say "Found multiple bugs, using bug " . colored( $bug_id, 'green' ) . ", skipping " . colored( $others, 'green' ) if $others && $verbose;
+        $bug_id =~ s/^\s+|\s+$//g;
+        $others =~ s/^\s+|\s+$//g;
 
-    unless ($bug_id) {
-        say colored( "Bug not found for ticket $ticket_id", 'red' )
-          if $verbose;
-        next;
-    }
-    sleep(4);    # Pause for another 4 seconds so we don't get banned from bugzilla
-    say "Found related bug " . colored( $bug_id, 'green' );
+        say "Found multiple bugs, using bug " . colored( $bug_id, 'green' ) . ", skipping " . colored( $others, 'green' ) if $others && $verbose;
 
-    my $bug = $koha_client->get_bug($bug_id);
+        unless ($bug_id) {
+            say colored( "Bug not found for ticket $ticket_id", 'red' )
+              if $verbose;
+            next;
+        }
+        sleep(4);    # Pause for another 4 seconds so we don't get banned from bugzilla
+        say "Found related bug " . colored( $bug_id, 'green' );
 
-    my $ticket_status = $ticket->{'CF.{Bug Workflow}'} || q{};
-    my $ticket_version = $ticket->{'CF.{Koha Version}'} || q{};
+        try {
+            my $bug = $koha_client->get_bug($bug_id);
 
-    my $bug_status = $bug->{status} || q{};
-    my $bug_version = $bug->{cf_release_version} || q{};
+            my $ticket_status = $ticket->{'CF.{Bug Workflow}'} || q{};
+            my $ticket_version = $ticket->{'CF.{Koha Version}'} || q{};
 
-    if ( $ticket_status ne $bug_status || $ticket_version ne $bug_version ) {
-	say "RT ticket status " . colored( $ticket_status, 'cyan' ) . " doesn't match community bug status " . colored( $bug_status, 'green' ) . ", updating RT ticket." if $verbose > 1;
-        $rt->edit(
-            type => 'ticket',
-            id   => $ticket_id,
-            set  => {
-                "CF.{Bug Workflow}" => $bug_status,
-                "CF.{Koha Version}" => $bug_version,
+            my $bug_status = $bug->{status} || q{};
+            my $bug_version = $bug->{cf_release_version} || q{};
+
+            if ( $ticket_status ne $bug_status || $ticket_version ne $bug_version ) {
+            say "RT ticket status " . colored( $ticket_status, 'cyan' ) . " doesn't match community bug status " . colored( $bug_status, 'green' ) . ", updating RT ticket." if $verbose > 1;
+                $rt->edit(
+                    type => 'ticket',
+                    id   => $ticket_id,
+                    set  => {
+                        "CF.{Bug Workflow}" => $bug_status,
+                        "CF.{Koha Version}" => $bug_version,
+                    }
+                );
+            } else {
+                say "Community bug and RT ticket status match: " . colored( $bug_status, 'yellow' ) . ", skipping update." if $verbose > 1;
             }
-        );
-    } else {
-        say "Community bug and RT ticket status match: " . colored( $bug_status, 'yellow' ) . ", skipping update." if $verbose > 1;
+        };
     }
 }
 
